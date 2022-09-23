0614101f762f3ad7d2d6c2c8578dabfba27d1218
jdhalmahapatra <jyotiraditya12@hotmail.com>
2022-09-23 16:28:10 +0530

    1. CRUD completed of Catalog.API 2. Containerize the Catalog.API and MongoDB using Docker-Compose

---
+++ src/Services/Catalog/Catalog.API/Controller/CatalogController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catalog.API.Entities;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : Controller
+    {
+
+        private readonly IProductRepository _repository;
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(IProductRepository repository, ILogger<CatalogController> logger)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(Exception));
+            _logger = logger ?? throw new ArgumentNullException(nameof(Exception));
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _repository.GetProducts();
+            return Ok(products);
+        }
+
+
+        [HttpGet ("{id:length(24)}", Name ="GetProduct")]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Product>> GetProductById(string id)
+        {
+            var product = await _repository.GetProductById(id);
+            if(product == null)
+            {
+                _logger.LogError($"Product with {id}, not found");
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+
+        [Route("[action]/{category}", Name ="GetProdutByCategory")]
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string category)
+        {
+            var products = await _repository.GetProductsByCategory(category);
+            return Ok(products);
+        }
+
+        [Route("[action]/{name}", Name ="GetProductByName")]
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByName(string name)
+        {
+            var products = await _repository.GetProductsByName(name);
+            return Ok(products);
+        }
+
+
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status201Created)]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            await _repository.CreateProduct(product);
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+        {
+           return Ok(await _repository.UpdateProduct(product));
+            
+        }
+
+        [HttpDelete("{id:length(24)}", Name ="DeleteProduct")]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        public async Task<IActionResult> DeleteProduct(string id)
+        {
+            return Ok(await _repository.DeleteProdcut(id));
+        }
+        
+    }
+}
+
---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        //For: Get All Products 
+        Task<IEnumerable<Product>> GetProducts();
+
+        //For: Get Procut by Id
+        Task<Product> GetProductById(string id);
+
+        //For: Get Products by Name
+        Task<IEnumerable<Product>> GetProductsByName(string name);
+
+        //For: Get Products by Category
+        Task<IEnumerable<Product>> GetProductsByCategory(string category);
+
+        //For: Create Prouct
+        Task CreateProduct(Product product);
+
+        //For: Udpate Product
+        Task<bool> UpdateProduct(Product product);
+
+        //For: Delete Product by Id
+        Task<bool> DeleteProdcut(string id);
+
+    }
+}
+
