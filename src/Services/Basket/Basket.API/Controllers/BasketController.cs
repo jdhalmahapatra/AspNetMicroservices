25fa102e1443c3b7751c9b60ab96bcd0cb530207
jdhalmahapatra <jyotiraditya12@hotmail.com>
2022-09-26 17:34:58 +0530

    Created Basket.API project. Implemented GetBasket, UpdateBasket, DeleteBasket REST API. Integrate Redis Cache Open Source Key-Value DB

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/[controller]")]
+    public class BasketController : Controller
+    {
+        private readonly IBasketRepository _repository;
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(IEnumerable<ShoppingCart>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket =  await _repository.GetBasket(userName);
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), StatusCodes.Status200OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket ([FromBody] ShoppingCart shoppingCart)
+        {
+            return Ok(await _repository.UpdateBasket(shoppingCart));
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            await _repository.DeleteBasket(userName);
+            return Ok();
+        }
+    }
+}
+
